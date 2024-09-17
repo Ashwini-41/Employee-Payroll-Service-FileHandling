f6b77db36f4a69a238b09f5aa9b2fc38e5bc517f
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-17 12:52:34 +0530

    Added : method to read and write data from employee payroll service

--- src/com/employeePayroll_IO/EmployeePayrollService.java
+++ src/com/employeePayroll_IO/EmployeePayrollService.java
@@ -1,13 +1,47 @@
 package com.employeePayroll_IO;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeePayrollService {
 	
 	public enum IOService {CONSOLE_IO , FILE_IO,DB_IO,REST_IO};
+    private List<EmployeePayrollData> employeePayrollList;
+	public EmployeePayrollService() {}
 
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+	  this.employeePayrollList = employeePayrollList;
+	}
 	
 	public static void main(String[] args) {
-	  System.out.println("Welcome to employee payroll service IO ");
-
+	   ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+       EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+       Scanner consoleInputReader = new Scanner(System.in);
+       employeePayrollService.readEmployeePayroll(consoleInputReader);
+       employeePayrollService.writeEmployeePayrollData();
+       
+       
 	}
+
+	private void writeEmployeePayrollData() {
+		System.out.println(" \nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+		
+	}
+
+	private void readEmployeePayroll(Scanner consoleInputReader) {
+		System.out.println("Enter Employeee ID: ");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter Employee name: ");
+		String name = consoleInputReader.next();
+		System.out.println("Enter Employee Salary: ");
+		double salary = consoleInputReader.nextDouble();
+		
+		employeePayrollList.add(new EmployeePayrollData(id,name, salary));
+		
+		
+	}
+	
+	
 
 }
