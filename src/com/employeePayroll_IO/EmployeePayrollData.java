7200c95c7d0ddbc7271f1979f7f9fd79ef4d7176
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-17 12:33:10 +0530

    Welcome message

---
+++ src/com/employeePayroll_IO/EmployeePayrollData.java
@@ -0,0 +1,42 @@
+package com.employeePayroll_IO;
+
+import java.time.LocalDate;
+
+public class EmployeePayrollData {
+    
+	public int employee_id;
+	public String name;
+	public double salary;
+	public LocalDate start;
+	
+	public EmployeePayrollData(Integer employee_id, String name , Double salary) {
+		this.employee_id = employee_id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public EmployeePayrollData(int employee_id, String name,double salary,LocalDate start) {
+		this(employee_id, name, salary);
+		this.start = start;
+	}
+	
+	@Override
+	public String toString() {
+		return "EmployeePayrollData [employee_id = " + employee_id + " , name = " + name + ", salary=" + salary + "]";
+	}
+	
+	/*
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		
+		EmployeePayrollData emp = (EmployeePayrollData) obj;
+		return employee_id == emp.employee_id && Double.compare(emp.salary, salary) == 0 && name.equals(emp.name);
+	}  */
+}
+
---
+++ src/com/employeePayroll_IO/EmployeePayrollService.java
@@ -0,0 +1,13 @@
+package com.employeePayroll_IO;
+
+public class EmployeePayrollService {
+	
+	public enum IOService {CONSOLE_IO , FILE_IO,DB_IO,REST_IO};
+
+	
+	public static void main(String[] args) {
+	  System.out.println("Welcome to employee payroll service IO ");
+
+	}
+
+}
